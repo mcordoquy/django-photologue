73b6a4a09a3826c28301c9a6b36e417f98c6a8bf
Marie Claire <mcordoquy@linovia.com>
2014-01-05 21:53:11 +0100

    use Pillow instead of PIL

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     ],
     install_requires = [
         'Django>=1.4',
-        'PIL',
+        'Pillow',
         'django-taggit',
     ],
     classifiers = ['Development Status :: 5 - Production/Stable',
